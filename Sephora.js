50db2ed2a352e018ed520e3c813faa71957b6927
Giovanna Melgares <giovannamelgares222@gmail.com>
2025-03-31 19:26:17 -0300

    primeiro commit

---
+++ Sephora.js
@@ -0,0 +1,6 @@
+//Tarefa (01/04) - Alterar fundo de um site -> Sephora
+
+function pintarFundo(){
+    let wrapper = document.getElementById('wrapper');
+    wrapper.style.backgroundColor = 'pink';
+}
